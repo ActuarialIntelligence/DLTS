f04336ba6322216b8c071504b2218f34760264bf
Rajah <rajah.kats@gmail.com>
2018-04-17 19:02:11 +0200

    Unstaged re commit

--- src/ActuarialIntelligence.DLTS.Domain/ActuarialIntelligence.DLTS.NoiseModels/Distributions/NormalDistribution.cs
+++ src/ActuarialIntelligence.DLTS.Domain/ActuarialIntelligence.DLTS.NoiseModels/Distributions/NormalDistribution.cs
@@ -2,9 +2,9 @@
 
 namespace ActuarialIntelligence.DLTS.NoiseModels.DIstributions
 {
-    public class NormalDistribution
+    public static class NormalDistribution
     {
-        public double NormalDistributionValueAt(double x,double mean, double variance)
+        public static double NormalDistributionValueAt(double x,double mean, double variance)
         {
             var firstTerm = (1 / Math.Sqrt(2 * Math.PI * variance));
             var secondTerm = Math.Pow(Math.E, ((-1) * Math.Pow((x - mean), 2)) / (2 * variance));
