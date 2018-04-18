e3e2b1a8fb2d0a619bd331e5ba76a9812e91d207
Rajah <rajah.kats@gmail.com>
2018-04-18 12:32:56 +0200

    Added EXE for testing purposes,
    Added Noise models classes

---
+++ src/ActuarialIntelligence.DLTS.Domain/ActuarialIntelligence.DLTS.EXE/Program.cs
@@ -0,0 +1,12 @@
+﻿using ActuarialIntelligence.DLTS.NoiseModels;
+
+namespace ActuarialIntelligence.DLTS.EXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Noise.EmulateNoiseOverNPeriods(300, 1.5, 0.01);
+        }
+    }
+}
--- src/ActuarialIntelligence.DLTS.Domain/ActuarialIntelligence.DLTS.NoiseModels/Noise.cs
+++ src/ActuarialIntelligence.DLTS.Domain/ActuarialIntelligence.DLTS.NoiseModels/Noise.cs
@@ -2,22 +2,36 @@
 using ActuarialIntelligence.DLTS.NoiseModels.DIstributions;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace ActuarialIntelligence.DLTS.NoiseModels
 {
-    public class Noise
+    public static class Noise
     {
-        public void EmulateNoiseOverNPeriods(int periods, double mean, double variance)
+
+        public static IList<Point<double, double>> EmulateNoiseOverNPeriods(int periods, double mean, double variance)
         {
-            var noisePoints = new List<Point<double, double>>(); 
+            var noisePoints = new List<Point<double, double>>();
+           
             for(int i=0; i<periods; i++)
             {
+                var time = DateTime.Now;
                 var rand = new Random();
                 var minusPlus = rand.Next(-1, 1);
-                var randomValue = rand.NextDouble()*mean* minusPlus;
+                var randomValue = rand.NextDouble() + mean* minusPlus;
                 noisePoints.Add(new Point<double, double>(i, 
                     NormalDistribution.NormalDistributionValueAt(randomValue, mean, variance)));
+
+                //We distinctly dislike the C# Timer Class
+                while(DateTime.Now<time.AddMilliseconds(200))
+                {
+                    Console.WriteLine("{0}..{1}..{2}", i,DateTime.Now, time.AddMilliseconds(200));
+                }
+                
             }
+            return noisePoints;
         }
+
     }
+
 }
